dfd3647bc472d9ee757f329e80ec9b58fa773a9f
Eduardo Alencar <alencareduardo17@gmail.com>
2024-02-14 15:34:11 -0300

    Incrementando o INNER JOIN

--- Criando-Constraints/banco.sql
+++ Criando-Constraints/banco.sql
@@ -113,3 +113,8 @@ SELECT * FROM contas;
 SELECT employees.first_name, employees.last_name, salaries.salary 
 FROM employees INNER JOIN salaries
 ON employees.emp_no = salaries.emp_no;
+
+SELECT employees.first_name, employees.last_name, salaries.salary AS salario 
+FROM employees INNER JOIN salaries
+ON employees.emp_no = salaries.emp_no
+WHERE salaries.salary >= 120000;
